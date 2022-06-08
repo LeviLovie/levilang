eaeaafb2f4eaef47eecdf17ec3d490c8d2b80e25
Lev111111 <leviiloviee@gmail.com>
2022-06-08 10:59:07 +0700

    Update comand.cpp
    start to read commands

--- sys/comand.cpp
+++ sys/comand.cpp
@@ -5,16 +5,28 @@ using namespace std;
 
 void com()
 {
-    string com;
+    string com, comDown;
     while (com != "end")
     {
+        com, comDown = "";
         cout << "levilang < ";
         cin >> com;
 
+        // down commands
         if (com == "clear")
         {
             cout << "\033[2J";
             gotoxy(0, 0);
+        }
+
+        // up commands
+        for (int i = 0; i < com.size(); i++)
+        {
+            if (com[i] != char(" "))
+            {
+                comDown += com[i];
+            }
+            
         }
     }
 }
